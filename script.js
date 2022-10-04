3603dfc8749ba34131fa5046108c9fb12f379133
tasuke22 <tasukesuke@gmail.com>
2022-10-04 12:53:09 +0900

    headerレスポンシブ

--- script.js
+++ script.js
@@ -29,27 +29,7 @@
 //   $(".js-drawer-btn").trigger("click");
 // });
 
-$(function () {
-  $(window).on("scroll", function () {
-    if (0 < $(this).scrollTop()) {
-      $(".js-header").addClass("none");
-      $(".js-underlayer-header").removeClass("none");
-      $(".js-underlayer-header").load("room.html .l-underlayer-header");
-    } else {
-      $(".js-header").removeClass("none");
-      $(".js-underlayer-header").addClass("none");
-    }
-  });
-});
-
-// $(function () {
-//   let select = $(".p-news__select > span");
-//   select.on("click", function () {
-//     let index = select.index($(this));
-//     $(".p-cards").removeClass("is-active").eq(index).addClass("is-active");
-//   });
-// });
-
+// タブの切替
 $(function () {
   let select = $(".p-news__select > span");
   select.on("click", function () {
@@ -57,4 +37,55 @@ $(function () {
     $(".p-cards").removeClass("is-active").eq(index).addClass("is-active");
     select.removeClass("is-active").eq(index).addClass("is-active");
   });
+});
+
+// ハンバーガーメニュー
+$(function () {
+  $(".js-drawer-btn").on("click", function () {
+    $(this).toggleClass("open");
+    $(".js-drawer-nav").toggleClass("open");
+  });
+});
+
+$(".header__list a[href]").on("click", function (event) {
+  $(".js-drawer-btn").trigger("click");
+});
+
+// なにをしたら横からでてくるんだっけ？
+
+$(window).on("scroll", function () {
+  if (50 < $(this).scrollTop()) {
+    $(".js-drawer-btn").addClass("black");
+    $(".p-header__title").addClass("black");
+    $(".js-header-logo").addClass("none");
+    $(".js-header-logo--sub").addClass("black");
+    $(".p-header").addClass("black");
+    $(".p-gnav__list").addClass("black");
+  } else {
+    $(".js-drawer-btn").removeClass("black");
+    $(".p-header__title").removeClass("black");
+    $(".js-header-logo").removeClass("none");
+    $(".js-header-logo--sub").removeClass("black");
+    $(".p-header").removeClass("black");
+    $(".p-gnav__list").removeClass("black");
+  }
+});
+
+$(window).on("scroll", function () {
+  if (
+    50 < $(window).scrollTop() &&
+    $(".p-gnav__list").hasClass("black") &&
+    $(".js-drawer-btn").hasClass("black") &&
+    $(".js-drawer-btn").hasClass("open")
+  ) {
+    $(".p-gnav__list").removeClass("black");
+    $(".js-drawer-btn").removeClass("black");
+  }
+});
+
+$(".js-drawer-btn").on("click", function () {
+  {
+    $(".p-gnav__list").removeClass("black");
+    $(".js-drawer-btn").removeClass("black");
+  }
 });
